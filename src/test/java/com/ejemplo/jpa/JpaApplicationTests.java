4fe1ad46f4b451fbe5af20a265f5f1ac891c743c
eduardoacunacoronado <edu191191@gmail.com>
2025-10-21 02:28:20 +0200

    Initial commit.

---
+++ src/test/java/com/ejemplo/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
